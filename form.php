7372829eb9d232537d270209c50d63c35aaf4f68
nikhilgogineni <79271875+nikhilgogineni@users.noreply.github.com>
2021-03-04 18:00:29 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+            your firstname is : <?php echo $_POST["fname"]; ?><br>
+            your lastname is : <?php echo $_POST["lname"]; ?><br>
+            your emailaddress is : <?php echo $_POST["emailid"]; ?><br>
+            your age is : <?php echo $_POST["age"]; ?><br>
+            your phone number is : <?php echo $_POST["phoneno"]; ?><br>
+            your date of birth is : <?php echo $_POST["dob"]; ?><br>
+    </body>
+</html>
